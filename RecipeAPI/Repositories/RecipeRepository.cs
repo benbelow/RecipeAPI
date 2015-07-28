76fe74006f6cd39603098b24b0784bb5be83586c
Ben Below <benjamin.below@gmail.com>
2015-07-28 23:09:58 +0100

    Add Entity Framework models.
    Added edmx database first models. Added base and recipe repository following the repository pattern.

---
+++ RecipeAPI/Repositories/RecipeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RecipeAPI.Models;
+using System.Data.Entity;
+
+namespace RecipeAPI.Repositories
+{
+    public interface IRecipeRepository : IRepository<Recipe>
+    {
+    }
+
+    public class RecipeRepository : Repository<Recipe>, IRecipeRepository
+    {
+        public RecipeRepository(DbContext context) : base(context) {}
+    }
+}
---
+++ RecipeAPI/Repositories/Repository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RecipeAPI.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        T Add(T entity);
+        void SaveContext();
+        IEnumerable<T> GetAll();
+        void UpdateEntity(T entity);
+    }
+
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext context;
+
+        protected Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        protected DbSet<T> Entities
+        {
+            get { return context.Set<T>(); }
+        }
+
+        public T Add(T entity)
+        {
+            Entities.Add(entity);
+            return entity;
+        }
+
+        public void SaveContext()
+        {
+            context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Entities.AsEnumerable();
+        }
+
+        public void UpdateEntity(T entity)
+        {
+            if (entity != null)
+            {
+                context.Entry(entity).Reload();
+            }
+        }
+    }
+}
