1f0f75e15338a0b8aefaef84d1f1c4508f56e278
Ben Below <benjamin.below@softwire.com>
2016-01-08 17:37:46 +0000

    Switch to OWIN startup.

---
+++ RecipeAPI/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeAPI
+{
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+//            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            WebApiConfig.Register(config);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
